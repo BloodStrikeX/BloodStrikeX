4abcb895d998c9d6e659d2e0f558d4dd523b3d7b
BloodStrikeFarsi <bloodstrikefarsi@gmail.com>
2025-05-02 09:57:14 +0330

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const toggleBtn = document.getElementById('menuToggle');
-  const navLinks  = document.getElementById('navLinks');
-
-  toggleBtn.addEventListener('click', () => {
-    toggleBtn.classList.toggle('active');
-    navLinks.classList.toggle('active');
-  });
-});
